201d4fa4db4697b4e5994ec981ab970adabe2310
ar77777 <game_aran_777@yahoo.co.jp>
2020-10-11 03:45:24 +0900

    投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:game_title, :platform, :time_zoon, :text, :call).merge(user_id: current_user.id)
+  end
 end
