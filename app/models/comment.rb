8161853707add6d0cefe36f91234e2ff037f9224
ar77777 <game_aran_777@yahoo.co.jp>
2020-10-28 18:33:33 +0900

    コメント機能実装

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+  #バリデーション
+  validates :content, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'rooms/index'
   devise_for :users
   root to: 'tops#index'
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :rooms, only: [:index, :create, :show] do
     resources :messages, only: [:create]
   end
