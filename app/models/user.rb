ac7e5e6b247da96c3d108c039ec2f25405eff2c3
ar77777 <game_aran_777@yahoo.co.jp>
2020-10-30 20:15:51 +0900

    新規登録のアクティブハッシュ化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -9,11 +11,16 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  belongs_to_active_hash :sex
+  belongs_to_active_hash :age
 
   # active_storage
   has_one_attached :image
 
+  # validation
   validates :nickname, :sex_id, :age_id, presence: true, unless: :was_attached?
+  validates :sex_id, numericality: { other_than: 1 } 
+  validates :age_id, numericality: { other_than: 1 } 
 
   def was_attached?
     image.attached?
--- db/migrate/20201010120735_devise_create_users.rb
+++ db/migrate/20201010120735_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ''
       t.string :nickname,           null: false
       t.integer :sex_id,            null: false
-      t.integer :age_id, null: false
+      t.integer :age_id,            null: false
 
       ## Recoverable
       t.string   :reset_password_token
