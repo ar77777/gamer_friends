a7a06027cf090cfdb07e32f8689561dad6b49cb4
ar77777 <game_aran_777@yahoo.co.jp>
2020-11-11 15:08:41 +0900

    ゲストログイン機能修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def new_guest
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
+      user.password = '1q2w3e'
       user.nickname = 'ゲスト'
       user.sex_id = 2
       user.age_id = 2
